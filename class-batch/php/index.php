27fff52a49ec28d86e7cc94b02d56eeeeb95a336
unknown <em.satyam@gmail.com>
2022-11-26 17:30:37 +0530

    Initial Commmit

--- class-batch/php/index.php
+++ class-batch/php/index.php
@@ -1,6 +1,6 @@
-<?php
+an<?php
 $a =45;
-$b =34;
+b =34;
 $c ="Ankit ";
 echo"You'r " .$c."<br />";
 echo $a." " ;
@@ -9,4 +9,6 @@ echo $b ."<br />";
 $r="satyam";
 $p="rupesh";
 echo $r."<br />";
+?>
 //this is my name with my friend >  
+    // that is one of the most important for me thats why i need thiss
