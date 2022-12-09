db34e27187138072564056857aeb180398e62aa1
unknown <em.satyam@gmail.com>
2022-12-09 23:54:37 +0530

    login_Form Intentities has been added

---
+++ project_2/output.php
@@ -0,0 +1,9 @@
+<?php
+$fname = $_GET['fname'];
+$lname = $_GET['lname'];
+$father_name = $_GET['father_name'];
+$aadhar_no = $_GET['aadhar_no'];
+$phone_no = $_GET['phone_no'];
+$school_name = $_GET['school_name'];
+$class = $_GET['class'];
+$addsubject = $_GET['addsubject'];
