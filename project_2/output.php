f2198cabb41fa0aac1b966c5766b1bcd403b5bbc
unknown <em.satyam@gmail.com>
2022-12-11 22:05:01 +0530

    Minor changes in php output file

--- project_2/output.php
+++ project_2/output.php
@@ -9,7 +9,9 @@ $class = $_GET['class'];
 $addsubject = $_GET['addsubject'];
  
 require_once("db/connection,php");
-$sth = $connection->prepare("INSERT INTO `your db table name`(fname, lname, father_name, aadhar_no, phone_no, school_name, class, addsubject)")
+$sth = $connection->prepare("INSERT INTO `your db table name`(fname, lname, father_name, aadhar_no, phone_no, school_name, class, addsubject)VALUE(:db_fname, :db_lname, :db_father_name, :db_aadhar_no, :db_phone_no, :db_school_name, :db_class, :db_addsubject)");
+
+
 
 
 
