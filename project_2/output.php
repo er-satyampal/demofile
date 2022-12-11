0edf94e2cadde8e960c221409caf3fc2ba90d94b
unknown <em.satyam@gmail.com>
2022-12-11 21:56:27 +0530

    Connection file atteching with the php output file

--- project_2/output.php
+++ project_2/output.php
@@ -8,4 +8,9 @@ $school_name = $_GET['school_name'];
 $class = $_GET['class'];
 $addsubject = $_GET['addsubject'];
  
+require_once("db/connection,php");
+$sth = $connection->prepare("INSERT INTO `your db table name`(fname, lname, father_name, aadhar_no, phone_no, school_name, class, addsubject)")
+
+
+
 ?>
