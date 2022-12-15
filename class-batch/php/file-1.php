31623862c33459f130e32cdd2b815fa88b187e01
unknown <em.satyam@gmail.com>
2022-12-15 20:56:22 +0530

    Php New File Added

---
+++ class-batch/php/file-1.php
@@ -0,0 +1,25 @@
+<?php
+$a =45;
+$b =34;
+$c ="Ankit ";
+echo"You'r " .$c."<br />";
+echo $a." " ;
+ //this is php file 
+echo $b ."<br />"; 
+$r="satyam";
+$p="rupesh";
+echo $r."<br />";
+//this is my name with my friend 
+$ankit = 123456574695278;
+$name =7475872454523787;
+echo $name ."<br/>";
+echo $ankit."<br/>";
+
+//this is one of most important programming language 
+ $var = "This is World";
+ echo $var."<br/>";
+ $var ="report";
+ echo $var;
+$vari="wow so nice";
+echo $vari;
+?>
