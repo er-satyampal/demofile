d2b7a5d0a330f360de0e07f6d0e0dfd95e55ad09
unknown <em.satyam@gmail.com>
2022-12-16 22:09:01 +0530

    Minor File Changes

--- class-batch/php/file-1.php
+++ class-batch/php/file-1.php
@@ -9,17 +9,4 @@ echo $b ."<br />";
 $r="satyam";
 $p="rupesh";
 echo $r."<br />";
-//this is my name with my friend 
-$ankit = 123456574695278;
-$name =7475872454523787;
-echo $name ."<br/>";
-echo $ankit."<br/>";
-
-//this is one of most important programming language 
- $var = "This is World";
- echo $var."<br/>";
- $var ="report";
- echo $var;
-$vari="wow so nice";
-echo $vari;
-?>
+//this is my name with my friend >
